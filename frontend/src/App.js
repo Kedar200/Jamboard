22596feeeded1a6a149b733627c9a7c8855d8fdb
Pranav-Khude <202151077@iiitvadodara.ac.in>
2024-05-16 19:56:34 +0530

    temp code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import Main from './Components/Main/Main.jsx';
+import Navbar from './Components/Navbar.jsx';
 
 
 function App() {
   return (
-    <Main/>
+    <> <Navbar/>
+    <Main/></>
   );
 }
 
--- frontend/src/Components/Main/Main.jsx
+++ frontend/src/Components/Main/Main.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Toolbar from './Toolbar.jsx';
 import Canvas from './Canvas.jsx';
-import './Main.css'
+import './Main.css';
+
 const Main = () => {
     const [cursor, setCursor] = useState('default');
 
     return (
-        <div className="main" style={{ cursor }}>
-            <Toolbar setCursor={setCursor} />
-            <Canvas />
+        <div className="main">
+            <Navbar />
+            <div className="content">
+                <Toolbar setCursor={setCursor} />
+                <div className="canvas-container">
+                    <Canvas cursor={cursor} />
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Main
+export default Main;
